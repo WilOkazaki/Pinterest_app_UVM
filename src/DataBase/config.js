c837dd0bab5ca8e7b67a5e02f48d1c88115863c7
Raulscj <92169825+Raulscj@users.noreply.github.com>
2023-02-21 12:35:15 -0400

    comentarios

--- src/DataBase/config.js
+++ src/DataBase/config.js
@@ -1,16 +1,21 @@
-require("./conexion");
+//Requerimiento de Express
 const express = require("express");
+//Requerimiento de modulo que comunica los servidores
+const cors = require("cors");
+//Inicio de Exprees
 const app = express();
-
+//Requerimiento de la conexion
+require("./conexion");
 //Settings
 app.set("appName", "Pinterest");
 app.set("port", 3030);
 
 //Middlewares
+app.use(cors());
 app.use(express.json());
 
-//Rutas
-app.use(require("./src/Routes/AppRoutes"));
+//Requerimiento y uso de Rutas
+app.use(require("./src/Routes/AppRoutes.jsx"));
 
 //Servidor
 app.listen(app.get("port"), () => {
